2c71745c96c8da50f88ea8a18d0c4d553ec5a888
丁二 <n872485238@gmail.com>
2022-05-12 20:36:14 +0800

    Fix url

--- Formula/contacts-phonetic.rb
+++ Formula/contacts-phonetic.rb
@@ -1,8 +1,8 @@
 class ContactsPhonetic < Formula
-  desc "Add phonetic names to contacts."
-  homepage "https://github.com/Elethom/contacts-phonetic"
-  url "https://github.com/Elethom/contacts-phonetic/archive/0.3.tar.gz"
-  sha256 "7d08bbfebabc3d1992d6bd66d452228b66ffec570e0250c0f659fd33f836908e"
+  desc "Add phonetic names to contacts. Crafted by Elethom originally."
+  homepage "https://github.com/Butanediol/contacts-phonetic"
+  url "https://github.com/Butanediol/contacts-phonetic/archive/0.3.tar.gz"
+  sha256 "cf9d04b484fc41837a5f86cade99bb60834a39b08aafa6848d33b6b1f88f1a7d"
 
   depends_on :macos => :el_capitan
 
